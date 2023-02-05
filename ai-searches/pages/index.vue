9f0673a26e8a0ad34bada0bc3dffe1bf0a0942a6
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-02-05 14:21:36 +0300

    many ui changes

--- ai-searches/pages/index.vue
+++ ai-searches/pages/index.vue
@@ -1,55 +1,66 @@
 <template>
-    <div>
-        <section class=" bg-gradient-to-b from-gray-900 via-slate-900 to-pink-900 md:overflow-hidden">
-        <div class=" flex md:justify-end md:items-center justify-start items-start text-white gap-5 space-x-6 mx-6 md:mx-32 py-3 md:py-6">
-        <NuxtLink
-        to="/" class="flex items-center gap-2">
-        <Icon name="teenyicons:home-solid" class="w-6 h-6" />
-         <span>Home</span>
-        </NuxtLink>
-        <NuxtLink
-        to="/" class="flex items-center gap-2">
-        <Icon name="teenyicons:search-circle-solid" class="w-6 h-6" />
-        <span>Search vids</span>
-        </NuxtLink>
-        <button class=" px-9 py-3 bg-red-600 rounded">Login</button>
-        </div>
-  <div
-    class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
-
-  >
-    <div class="mx-auto max-w-xl text-center">
-      <h1 class="text-3xl font-extrabold sm:text-5xl text-white">
-        Understand Search Flow.
-        <strong class="font-extrabold text-red-700 sm:block">
-          Increase Conversion.
-        </strong>
-      </h1>
-
-      <p class="mt-4 sm:text-xl sm:leading-relaxed text-blue-400">
-       Performant search. Searches for the best Movies using the Movie Database API.
-      </p>
-
-      <div class="mt-8 flex flex-wrap justify-center gap-4">
-        <a
-          class="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-          href="/get-started"
-        >
-          Get Started
-        </a>
-
-        <a
-          class="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto ring-1 ring-white"
-          href="/about"
-        >
-          Learn More
-        </a>
+<!-- component -->
+<div>
+  <div class="h-screen flex justify-center items-center bg-cover bg-center" style="background-image: linear-gradient(rgb(0 0 0 / 60%), rgb(0 0 0 / 60%)), url('https://assets.nflxext.com/ffe/siteui/vlv3/c0a32732-b033-43b3-be2a-8fee037a6146/2fe6e3c0-5613-4625-a0c1-3d605effd10b/IN-en-20210607-popsignuptwoweeks-perspective_alpha_website_large.jpg')">
+  
+    <div class="space-y-5 bg-gradient-to-br from-slate-900 to-pink-800 p-10 translate-x-5 transform scale-95 animate-pulse duration-500">
+      <div class="text-white font-bold text-2xl md:text-5xl  flex justify-start items-center m-0 md:mx-32 flex-col">
+        <span>Unlimited movies</span>
+        <span>shows and more.</span>
       </div>
+     
+      <p class="text-white text-lg flex flex-col items-center">
+      <span>Readily Performant </span>
+        <span class=" text-orange-600  text-xl flex flex-col items-center font-extrabold">Search with our api.</span>
+      
+      </p>
+   
+
     </div>
   </div>
-</section>
-
+  <hr />
+  <div class="h-screen flex items-center justify-center bg-black p-5 md:flex-row flex-col">
+    <div class="space-y-5 p-5">
+      <p class="text-white font-bold text-4xl">Enjoy on your TV.</p>
+      <p class="text-white font-semiboldbold text-xl">Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</p>
     </div>
+    <div>
+      <img alt="TV" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" />
+    </div>
+  </div>
+  <hr />
+  <div class="h-screen flex items-center justify-center bg-black p-5 md:flex-row flex-col">
+    <div>
+      <img alt="" class="our-story-card-img" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" data-uia="our-story-card-img" />
+    </div>
+    <div class="space-y-5">
+      <p class="text-white font-bold text-4xl">Download your shows to watch offline.</p>
+      <p class="text-white font-semiboldbold text-xl">Save your favourites easily and always have something to watch.</p>
+    </div>
+  </div>
+  <hr />
+  <div class="h-screen flex items-center justify-center bg-black p-5 md:flex-row flex-col">
+    <div class="space-y-5 p-5">
+      <p class="text-white font-bold text-4xl">Watch everywhere.</p>
+      <p class="text-white font-semiboldbold text-xl">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
+    </div>
+    <div>
+      <img alt="" class="our-story-card-img" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile-in.png" data-uia="our-story-card-img" />
+    </div>
+  </div>
+  <hr />
+  <div class="h-screen flex items-center justify-center bg-black p-5 md:flex-row flex-col">
+    <div class="p-5">
+      <img alt="" class="our-story-card-img" src="https://occ-0-5556-3662.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVxdX2WnFSp49eXb1do0euaj-F8upNImjofE77XStKhf5kUHG94DPlTiGYqPeYNtiox-82NWEK0Ls3CnLe3WWClGdiJP.png?r=5cf" data-uia="our-story-card-img" />
+    </div>
+    <div class="space-y-5">
+      <p class="text-white font-bold text-4xl">Create profiles for children.</p>
+      <p class="text-white font-semiboldbold text-xl">Send children on adventures with their favourite characters in a space made just for them—free with your membership.</p>
+    </div>
+  </div>
+  <hr />
+
+</div>
 </template>
 
 <script lang="ts">
---
+++ ai-searches/pages/search.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+<!-- component -->
+<div class="w-full max-w-screen-xl mx-auto px-6">
+        <div class="flex justify-center p-4 px-3 py-10">
+            <div class="w-full max-w-md">
+                <div class="bg-white shadow-md rounded-lg px-3 py-2 mb-4">
+                    <div class="block text-gray-700 text-lg font-semibold py-2 px-2">
+                        Item List
+                    </div>
+                    <div class="flex items-center bg-gray-200 rounded-md">
+                        <div class="pl-2">
+                            <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path class="heroicon-ui"
+                                    d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                            </svg>
+                        </div>
+                        <input
+                            class="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
+                            id="search" type="text" placeholder="Search teams or members">
+                    </div>
+                    <div class="py-3 text-sm">
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">Tighten Co.</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Team</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">Taylor Otwell</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Member</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">Adam Wathan</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Member</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">Duke Street Studio Inc.</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Team</div>
+                        </div>
+                        <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                            <span class="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
+                            <div class="flex-grow font-medium px-2">Jeffrey Wey</div>
+                            <div class="text-sm font-normal text-gray-500 tracking-wide">Member</div>
+                        </div>
+                    </div>
+                    <div class="block bg-gray-200 text-sm text-right py-2 px-3 -mx-3 -mb-2 rounded-b-lg">
+                        <button class="hover:text-gray-600 text-gray-500 font-bold py-2 px-4">
+                            Cancel
+                        </button>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Invite
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
